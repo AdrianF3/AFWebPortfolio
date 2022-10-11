05adc67af4d6f665f753d787edd5d2569384899e
AdrianF3 <AF1219@gmail.com>
2022-10-11 15:11:49 -0600

    Begin new verison of portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <container>
+        {/* Header / Welcome Area */}
+          {/* anchor links to jump to certain parts of the page - also include button to toggle dark mode */}
+        <section className='h-40 bg-emerald-800 text-center text-white'>
+          <div className='flex flex-row justify-evenly'>
+            {/* row section one, name and summary */}
+            <div className='flex flex-col'>
+              <p className='mt-6 text-3xl'>
+                Adrian Fregoso
+              </p>
+              <p className='text-xl mt-4'>
+                Software Developer
+              </p>
+            </div>
+            {/* row section two, light/dark mode toggle */}
+            <div className='flex justify-items-end'>
+              <p>light</p>
+              <p>dark</p>
+
+            </div>
+          </div>
+        </section>
+
+
+
+        {/* About Me */}
+        <section>
+          <div>
+            <p></p>
+          </div>
+        </section>      
+        {/* Developer Experience & Projects */}
+          {/* Full Projects followed by mini projects/components to use on page only */}
+        <section>
+          <div>
+            <p></p>
+          </div>
+        </section>
+        {/* Updated Resume & Cover & contact info */}
+          {/* Simple section, easy to download from */}
+        <section>
+          <div>
+            <p></p>
+          </div>
+        </section>
+
+
+
+      </container>      
     </div>
   );
 }
