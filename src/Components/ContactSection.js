05cc7d128ce4bee879fa4e06233da2d643224daf
AdrianF3 <AF1219@gmail.com>
2022-11-04 20:14:22 -0600

    finished rough layout of navbar

--- src/Components/ContactSection.js
+++ src/Components/ContactSection.js
@@ -3,54 +3,46 @@ import linkedInSVG from '../images/linkedIn.svg'
 import githubSVG from '../images/githubMinisvg.svg'
 
 export default function ContactSection() {
-  return (
-    <section className="text-gray-600 body-font relative p-4">
-      <div className="container px-5 py-24 mx-auto border-cyan-900 border-4 bg-cyan-900/30 rounded-xl">
-        
-        <div className="flex flex-col text-center w-full mb-8">
-          <h2 className='tracking-widest text-4xl uppercase'>Adrian Fregoso</h2>
-          <p className="leading-normal my-5">
-              Located in <span className='tracking-wider font-bold underline underline-offset-2'>Denver, Colorado</span>
-          </p>
-          <div>
-            <button
-              className='p-3 bg-cyan-700 rounded-xl text-white text-xl text-wider'
-            >
-              Download My Resume &amp; Cover Letter (.zip)
-            </button>
-          </div>      
-      </div>
+    return (
+        <section className="text-gray-600 body-font relative p-4">
+            <div
+                className="container px-5 py-24 mx-auto border-cyan-900 border-4 bg-cyan-900/30 rounded-xl">
 
-    <div className="lg:w-1/2 md:w-2/3 mx-auto">
-        <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center text-xl">  
-          <div>
-            <a href='mailto:AdrianF.WebDev@gmail.com' className="text-indigo-500">AdrianF.WebDev@gmail.com</a>
-          </div>                  
-          <div>
-            <a href='tel:3102921219' target='_blank' rel='noreferrer'>(310) 292-1219</a>          
-          </div>
-        </div>
-          <div className='flex flex-row justify-center gap-4'>
-            <a href='https://github.com/AdrianF3' target='_blank'>
-              <img 
-                className='h-10 m-auto'
-                src={linkedInSVG} 
-                alt='LinkedIn'
-              />          
-            </a>
-            <a href='http://www.linkedin.com/in/adrian-fregoso' target='_blank'>
-              <img 
-                className='h-10 m-auto'
-                src={githubSVG} 
-                alt='LinkedIn'
-              />          
-            </a>
-          </div>
-      </div> 
+                <div className="flex flex-col text-center w-full mb-8">
+                    <h2 className='tracking-widest text-4xl uppercase'>Adrian Fregoso</h2>
+                    <p className="leading-normal my-5">
+                        <span className='tracking-wider font-bold underline underline-offset-3'>Denver, Colorado</span>
+                    </p>
+                    <div>
+                        <button className='p-3 bg-cyan-700 rounded-xl text-white text-xl text-wider'>
+                            Download My Resume &amp; Cover Letter (.zip)
+                        </button>
+                    </div>
+                </div>
 
-    </div>
+                <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                    <div
+                        className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center text-xl">
+                        <div>
+                            <a href='mailto:AdrianF.WebDev@gmail.com' className="text-indigo-500">AdrianF.WebDev@gmail.com</a>
+                        </div>
+                        <div>
+                            <a href='tel:3102921219' target='_blank' rel='noreferrer'>(310) 292-1219</a>
+                        </div>
+                    </div>
+                    <div className='flex flex-row justify-center gap-4'>
+                        <a href='https://github.com/AdrianF3' target='_blank'>
+                            <img className='h-10 m-auto' src={linkedInSVG} alt='LinkedIn'/>
+                        </a>
+                        <a href='http://www.linkedin.com/in/adrian-fregoso' target='_blank'>
+                            <img className='h-10 m-auto' src={githubSVG} alt='LinkedIn'/>
+                        </a>
+                    </div>
+                </div>
 
-</section>
+            </div>
 
-  )
+        </section>
+
+    )
 }
--- src/Components/HeaderNavigation.js
+++ src/Components/HeaderNavigation.js
@@ -4,12 +4,54 @@ import {NavLink} from 'react-router-dom'
 export default function HeaderNavigation() {
     const [isMobileMenuOpen,
         setIsMobileMenuOpen] = useState(false)
+    const [isPortfolioOpen,
+        setIsPortfolioOpen] = useState(false)
 
     const defaultMenu = (
-        <div className='hidden md:flex flex-row gap-4'>
+        <div className='hidden md:flex flex-row gap-4 md:mr-4 my-auto'>
             <NavLink to='/'>
                 <p className=''>Home</p>
             </NavLink>
+            <NavLink to=''>
+                <p>Get In Touch</p>
+            </NavLink>
+            <div className='flex flex-col justify-center text-center'>
+                <button
+                    onClick={() => {
+                    isPortfolioOpen
+                        ? setIsPortfolioOpen(false)
+                        : setIsPortfolioOpen(true)
+                }}>
+                    <p className='font-bold uppercase'>Portfolio Projects</p>
+                </button>
+
+                {isPortfolioOpen
+                    ? <> <div className='border-2 border-gray-700 border-t-0 rounded-b-xl p-2'>
+                        <ul>
+                            <NavLink to='/weather'>
+                                <p className=''>Weather Project</p>
+                            </NavLink>
+                            <NavLink to='ytcontroller'>
+                                <p>YouTube Controller</p>
+                            </NavLink>
+                            <NavLink to=''>
+                                <p>Ector Grow</p>
+                            </NavLink>
+                        </ul>
+                    </div> </> : null}
+            </div>
+        </div>
+    )
+
+    const mobileMenu = (
+        <div className='flex flex-col justify-center text-center gap-2 py-2'>
+            <NavLink to='/'>
+                <p className=''>Home</p>
+            </NavLink>
+            <NavLink to=''>
+                <p>Get In Touch</p>
+            </NavLink>
+            <p className='font-bold uppercase border-b-2 border-white'>Portfolio Projects</p>
             <NavLink to='/weather'>
                 <p className=''>Weather Project</p>
             </NavLink>
@@ -19,41 +61,25 @@ export default function HeaderNavigation() {
             <NavLink to=''>
                 <p>Ector Grow</p>
             </NavLink>
-            <NavLink to=''>
-                <p>Get In Touch</p>
-            </NavLink>
         </div>
-    )
-
-    const mobileMenu = (
-        <ul className="md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-            <li>
-                <a className="hover:text-gray-200" href="#">HomeMM</a>
-            </li>
-            <li>
-                <a className="hover:text-gray-200" href="#">Portfolio ProjectsMM</a>
-            </li>
-            <li>
-                <a className="hover:text-gray-200" href="#">Ector GrowMM</a>
-            </li>
-            <li>
-                <a className="hover:text-gray-200" href="#">Get In TouchMM</a>
-            </li>
-        </ul>
     )
 
     // https://tailwindcomponents.com/component/navbar-hamburger-menu-for-ecommerce
     console.log('isMobileMenuOpen', isMobileMenuOpen)
 
-    return ( <> <div className="flex flex-wrap place-items-center h-fit">
+    return ( <> <div className="flex flex-wrap place-items-center h-fit ">
         <section className="relative mx-auto">
             {/* <!-- navbar --> */}
             <nav className="flex justify-between bg-gray-900 text-white w-screen">
                 <div className="py-6 flex justify-between w-full">
-                    <a className="text-3xl font-bold font-heading px-5" href="#">
-                        {/* <!-- <img class="h-9" src="logo.png" alt="logo"> --> */}
-                        Adrian Fregoso
-                    </a>
+                    {/* <a className="text-3xl font-bold font-heading px-5" href="#">
+                    </a> */}
+                    <NavLink to='/'>
+                        <h3
+                            className='text-2xl p-2 tracking-wider uppercase m-auto md:ml-10 text-center'>
+                            Adrian Fregoso
+                        </h3>
+                    </NavLink>
 
                     {/* <!-- Nav Links --> */}
                     {/* Full Display */}
@@ -66,13 +92,16 @@ export default function HeaderNavigation() {
                 </div>
                 {/* <!-- Open/Close Menu --> */}
                 <div
-                    className=' md:hidden'
+                    className='md:hidden p-3 transition duration-150 ease-in-out'
                     onClick={() => {
                     isMobileMenuOpen
                         ? setIsMobileMenuOpen(false)
                         : setIsMobileMenuOpen(true)
                 }}>
-                    <i className='material-icons'>menu</i>
+                    {!isMobileMenuOpen
+                        ? <i className='material-icons'>menu</i>
+                        : <i className='material-icons'>close</i>
+}
                 </div>
             </nav>
 
