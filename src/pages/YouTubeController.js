2dd2f578a5ed6b83ac895ad613fafb967209dde8
AdrianF3 <AF1219@gmail.com>
2022-11-08 21:35:22 -0700

    working on yt page

--- src/pages/YouTubeController.js
+++ src/pages/YouTubeController.js
@@ -4,7 +4,48 @@ import HeaderNavigation from '../components/HeaderNavigation'
 
 export default function YouTubeController() {
   // const [ youtubeURL, setYoutubeURL ] = useState('https://www.youtube.com/embed/woD6RYptp58?')  
-  const [ youtubeURL, setYoutubeURL ] = useState('https://www.youtube.com/embed/EUTo1urxwPo?')  
+  const [ youtubeURL, setYoutubeURL ] = useState('https://www.youtube.com/embed/EUTo1urxwPo?')
+  
+  const videoMoments = [
+    {
+      title: 'End of Ads',
+      description: 'a brief description',
+      timestamp: '2:58',
+      urlToPlay: 'https://www.youtube.com/embed/EUTo1urxwPo?start=178'
+    },
+    {
+      title: 'Tales of Omaha Beach',
+      description: 'a brief description',
+      timestamp: '9:55',
+      urlToPlay: 'https://www.youtube.com/embed/EUTo1urxwPo?start=595'
+    },
+    {
+      title: 'Tales of Sword Beach',
+      description: 'a brief description',
+      timestamp: '11:51',
+      urlToPlay: 'https://www.youtube.com/embed/EUTo1urxwPo?start=711'
+    },
+    {
+      title: 'Tales of Utah Beach',
+      description: 'a brief description',
+      timestamp: '13:55',
+      urlToPlay: 'https://www.youtube.com/embed/EUTo1urxwPo?start=835'
+    },
+    {
+      title: 'Tales of Gold Beach',
+      description: 'a brief description',
+      timestamp: '18:28',
+      urlToPlay: 'https://www.youtube.com/embed/EUTo1urxwPo?start=1108'
+    },
+    {
+      title: 'Tales of Juno Beach',
+      description: 'a brief description',
+      timestamp: '20:12',
+      urlToPlay: 'https://www.youtube.com/embed/EUTo1urxwPo?start=1212'
+    },
+  ]
+
+
   return (
     <>
     <HeaderNavigation/>
@@ -32,9 +73,9 @@ export default function YouTubeController() {
         <section className='flex flex-col w-10/12 mx-auto'>
 
           {/* YT Page Header Description */}
-          <div className='flex flex-row justify-center'>
+          <div className='flex flex-col md:flex-row justify-center'>
             <h2 className='w-3/12 text-3xl py-2 m-auto'>Armchair Historian</h2>
-            <p className='w-7/12 m-2 p-2 border-2 border-slate-700 rounded-xl'>
+            <p className='w-7/12 m-2 p-2 border-2 border-slate-700 rounded-xl mx-auto'>
               Hi, I'm Griffin Johnsen, founder of The Armchair Historian LLC and Armchair Historian channel.
               The Armchair Historian LLC specializes in producing educational and entertaining animated history videos.
               For more history animations go to https://armchairhistory.tv/
@@ -80,115 +121,37 @@ export default function YouTubeController() {
               />                                    
             </div>
             {/* Buwttons To Control Video Position */}
-            <section className='bg-slate-600 rounded-b-xl'>
-              <h2 className='text-white text-2xl p-2 tracking-wide'>Featured Video Moments</h2>
-            
-              <div className='grid grid-cols-2 md:grid-cols-3 p-2 gap-4'>
-
-                {/* Card 1 */}
-                <div className='bg-gray-300 rounded-xl p-2'>
-                  <div className='flex w-fit px-2 -mt-6  bg-sky-200 rounded-xl'>
-                    <p className='p-1 text-sm font-bold'>2:23</p>
-                  </div>
-                  {/* Title in bubble above card */}
-                  <div className='flex flex-row'>
-                    <h3 className='text-2xl text-center py-2 border-spacing-2 border-b-2 border-slate-700'>Tales of Omaha Beach</h3>
+            <section className='bg-slate-600 rounded-b-xl py-8'>
+              <h2 className='text-white text-2xl p-2 pb-5 tracking-wide'>Featured Video Moments</h2>          
+              <div className='grid grid-cols-2 md:grid-cols-3 p-2 gap-6'>
+                {videoMoments.length > 0 ? videoMoments.map((moment) => <>
+                  {/* Card 1 */}
+                  <div className='bg-gray-300 rounded-xl p-2'>
+                    <div className='flex w-fit px-2 -mt-6  bg-sky-200 rounded-xl'>
+                      <p className='p-1 text-sm font-bold'>{moment.timestamp}</p>
+                    </div>
+                    {/* Title in bubble above card */}
+                    <div className='flex flex-row'>
+                      <h3 className='text-2xl text-center py-2 border-spacing-2 border-b-2 border-slate-700'>{moment.title}</h3>
+                    </div>                  
+                    {/* brief description */}
+                    <div className='py-4'>
+                      <p>
+                        {moment.description}
+                      </p>
+                    </div>
+                    {/* Button to skip to timestamp */}
+                    <div className='flex justify-center'>
+                      <button 
+                        className='p-2 bg-emerald-500 rounded-xl'
+                        onClick={() => setYoutubeURL(moment.urlToPlay)}
+                      >
+                        Play This
+                      </button>
+                    </div>
                   </div>                  
-                  {/* brief description */}
-                  <div className='py-4'>
-                    <p>
-                      Brief Description of video feature
-                    </p>
-                  </div>
-                  {/* Button to skip to timestamp */}
-                  <div className='flex justify-center'>
-                    <button className='p-2 bg-emerald-500 rounded-xl'>
-                      Play This
-                    </button>
-                  </div>
-
-                </div>
-
-                {/* Card 1 */}
-                <div className='bg-gray-300 rounded-xl p-2'>
-                  {/* Title in bubble above card */}
-                  <div>
-                    <h3>Tales of Sword Beach</h3>
-                  </div>                  
-                  {/* brief description */}
-                  <div>
-                    <p>
-                      Brief Description of video feature
-                    </p>
-                  </div>
-                  {/* Button to skip to timestamp */}
-                  <div>
-                    <button className='p-2 bg-emerald-500 rounded-xl'>
-                      Play This in Video
-                    </button>
-                  </div>
-
-                </div>
-
-                {/* Card 3 */}
-                <div className='bg-gray-300 rounded-xl p-2'>
-                  {/* Title in bubble above card */}
-                  <div>
-                    <h3>Tales of Utah Beach</h3>
-                  </div>                  
-                  {/* brief description */}
-                  <div>
-                    <p>
-                      Brief Description of video feature
-                    </p>
-                  </div>
-                  {/* Button to skip to timestamp */}
-                  <div>
-                    <button className='p-2 bg-emerald-500 rounded-xl'>
-                      Play This in Video
-                    </button>
-                  </div>
-                </div>
-                {/* Card 4 */}
-                <div className='bg-gray-300 rounded-xl p-2'>
-                  {/* Title in bubble above card */}
-                  <div>
-                    <h3>Tales of Gold Beach</h3>
-                  </div>                  
-                  {/* brief description */}
-                  <div>
-                    <p>
-                      Brief Description of video feature
-                    </p>
-                  </div>
-                  {/* Button to skip to timestamp */}
-                  <div>
-                    <button className='p-2 bg-emerald-500 rounded-xl'>
-                      Play This in Video
-                    </button>
-                  </div>
-                </div>
-                {/* Card 5 */}
-                <div className='bg-gray-300 rounded-xl p-2'>
-                  {/* Title in bubble above card */}
-                  <div>
-                    <h3>Tales of Juno Beach</h3>
-                  </div>                  
-                  {/* brief description */}
-                  <div>
-                    <p>
-                      Brief Description of video feature
-                    </p>
-                  </div>
-                  {/* Button to skip to timestamp */}
-                  <div>
-                    <button className='p-2 bg-emerald-500 rounded-xl'>
-                      Play This in Video
-                    </button>
-                  </div>
-                </div>
-
-            </div>
+                </>) : null }        
+              </div>
             </section>
 
           </section>        
